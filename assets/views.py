84855d905338194fef3121185d433a94764e6f8f
zhang852469 <710567585@qq.com>
2018-07-19 13:12:11 +0800

    Initial commit

---
+++ assets/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from . import models
+from . import asset_handler
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
+
+@csrf_exempt
+def report(request):
+    """
+    通过csrf_exempt装饰器，跳过Django的csrf安全机制，让post的数据能被接收
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        asset_data = request.POST.get('asset_data')
+        data = json.loads(asset_data)
+        # 各种数据检查
+        if not data:
+            return HttpResponse("没有数据！")
+        if not issubclass(dict, type(data)):
+            return HttpResponse("数据必须为字典格式！")
+        # 是否携带了关键的sn号
+        sn = data.get('sn', None)
+        if sn:
+            # 进入审批流程
+            # 首先判断是否在上线资产中存在该sn
+            asset_obj = models.Asset.objects.filter(sn=sn)
+            if asset_obj:
+                # 进入已上线资产的数据更新流程
+                update_asset = asset_handler.UpdateAsset(request, asset_obj[0], data)
+                return HttpResponse("资产数据已经更新！")
+            else:   # 如果已上线资产中没有，那么说明是未批准资产，进入新资产待审批区，更新或者创建资产。
+                obj = asset_handler.NewAsset(request, data)
+                response = obj.add_to_new_assets_zone()
+                return HttpResponse(response)
+        else:
+            return HttpResponse("没有资产sn序列号，请检查数据！")
+
+def index(request):
+    assets = models.Asset.objects.all()
+    return render(request, 'assets/index.html', locals())
+def hashboard(request):
+    total = models.Asset.objects.count()
+    upline = models.Asset.objects.filter(status=0).count()
+    offline = models.Asset.objects.filter(status=1).count()
+    unknown = models.Asset.objects.filter(status=2).count()
+    breakdown = models.Asset.objects.filter(status=3).count()
+    backup = models.Asset.objects.filter(status=4).count()
+    up_rate = round(upline/total*100)
+    o_rate = round(offline/total*100)
+    un_rate = round(unknown/total*100)
+    bd_rate = round(breakdown/total*100)
+    bu_rate = round(backup/total*100)
+    server_number = models.Server.objects.count()
+    networkdevice_number = models.NetworkDevice.objects.count()
+    storagedevice_number = models.StorageDevice.objects.count()
+    securitydevice_number = models.SecurityDevice.objects.count()
+    software_number = models.Software.objects.count()
+    return render(request, 'assets/hashboard.html', locals())
+
+def detail(request):
+    """
+    资产详细
+    :param request:
+    :return:
+    """
+    asset = get_object_or_404(models.Asset, id=asset_id)
+    return render(request, 'assests/detail.html', locals())
